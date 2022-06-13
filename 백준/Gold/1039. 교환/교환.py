1cb20945f7eca1f59c191f0eb35b77fc9f7b35ce
Dev-Guccin <52245525+Dev-Guccin@users.noreply.github.com>
2022-06-13 14:33:44 +0900

    [Gold III] Title: 교환, Time: 112 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.py
+++ 백준/Gold/1039. 교환/교환.py
@@ -1,42 +1,37 @@
-"""
-데터가 작고 K가 10 이하이므로
-완탐으로 풀린다. 따라서 BFS로 매번 모든 경우를 찾고 해당 값 중에 가장 큰것을 선택하여 갱신
-매번 최대를 구하는게 아니라 K번실행하는 경우의 최댓값을 구해야한다.
-"""
 from collections import deque
 
-N, K = input().split()
-N = list(N)
-K = int(K)
-_N = list(N)
-answer = 0
 
+def BFS(N, K):
 
-def BFS(k, start):
+    q = deque([N])
 
-    q = deque([int("".join(start))])
-    count = 0
     while q:
-        if count == k:
-            return max(q)
-        setq = set()
+        S = set()
+        # print(q)
+        if K == 0:
+            return q
         for _ in range(len(q)):
-            target = q.popleft()
-            target = list(str(target))
+            num = q.popleft()
+
             for i in range(len(N)):
                 for j in range(i+1, len(N)):
-                    tmp = list(target)
-                    tmp[i] = target[j]
-                    tmp[j] = target[i]
+                    tmp = list(num)
+                    tmp[i] = num[j]
+                    tmp[j] = num[i]
                     if tmp[0] == '0':
                         continue
-                    setq.add(int("".join(tmp)))
-        q += (list(setq))
-        count += 1
+                    S.add("".join(tmp))
+        # print(S)
+        q += list(S)
+        K -= 1
+    return q
 
 
-tmp = BFS(K, _N)
-if tmp == None:
+N, K = map(int, input().split())
+tmp = BFS(str(N), K)
+# print(tmp)
+if len(tmp) == 0:
     print(-1)
 else:
-    print(tmp)
+    tmp = list(map(int, tmp))
+    print(max(tmp))
